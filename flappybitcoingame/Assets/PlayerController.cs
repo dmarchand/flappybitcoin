9e6d01796b7426d38ee5226483bf4e55f0ca36d0
Dan Marchand <daniel@dans-mbp.cable.rcn.com>
2014-02-10 18:41:40 -0500

    Movement 

---
+++ flappybitcoingame/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public float framesPerSecond;
+	public float forcePerClick;
+	
+	private SpriteRenderer _spriteRenderer;
+	private Rigidbody2D _rigidBody;
+	private bool _isMovingUp;
+	
+	// Use this for initialization
+	void Start () {
+		_spriteRenderer = renderer as SpriteRenderer;
+		_isMovingUp = false;
+		_rigidBody = this.rigidbody2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+		index = index % sprites.Length;
+		_spriteRenderer.sprite = sprites[ index ];
+
+		print(_rigidBody.velocity.y);
+
+		if (Input.GetButton ("Fire1")) {
+			this._rigidBody.velocity = new Vector2(0, forcePerClick * 10);
+			_isMovingUp = true;
+		}
+
+		if (_isMovingUp && this._rigidBody.velocity.y < .05f) {
+			_isMovingUp = false;
+		}
+	}
+}
